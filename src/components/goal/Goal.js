fb095847665d09799186ea958e86250d6541e8cb
hakase-monoshiri <ebenaeleazer@outlook.com>
2022-04-08 22:20:15 -0400

    move goal display contents to look better on mobile

--- src/components/goal/Goal.js
+++ src/components/goal/Goal.js
@@ -20,21 +20,23 @@ class Goal extends Component {
         <div className="goal" id={this.props.goal.id}>
             <div className="goal-title">
                 <h2>{this.props.goal.name}</h2>
-                <GoalProgressBar percentComplete={this.props.goal.percent_complete} />
             </div>
             <div className="goal-details">
-
                 <button className="goal-delete-button" onClick={() => this.props.delete(this.props.goal.id)} >X </button>
 
                 <h4> Description:</h4>
                 <p> {this.props.goal.description}</p>
+
+                <br/>
                 
             </div>
-                
+
+            <GoalProgressBar percentComplete={this.props.goal.percent_complete} />
            
             <button onClick={this.showHideTasks} className='task-display-button'> {this.state.activeTasks? 'Hide Tasks': 'Show Tasks'} </button>
            
             {this.state.activeTasks ? < TaskContainer  goalId={this.props.goal.id}/> : null}
+
             
 
         </div>
